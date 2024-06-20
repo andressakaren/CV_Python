cfdd3fe959635ae592d8efc8381b33af3c6a34d7
andressakaren <160805915+andressakaren@users.noreply.github.com>
2024-06-20 19:51:03 -0300

    add ex056.py

--- Python–Mundo_2/ex056.py
+++ Python–Mundo_2/ex056.py
@@ -1,8 +1,29 @@
-# Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas.
-# No final do programa, mostre: a média de idade do grupo,
-# qual é o nome do homem mais velho e quantas mulheres têm menos de 20 anos.
-
 def main():
-    ...
+    contMaior20 = 0
+    nomeMaisvelho = ''
+    maiorIdade = 0
+    somaIdades = 0
+    for n in range(1, 5):
+        print(f'____________________{n}° PESSOA____________________')
+        nome = str(input('Nome: '))
+        idade = int(input('Idade: '))
+        sexo = str(input('Sexo [F] - Feminino OU [M] - Masculino: ')).strip().upper()
+        somaIdades += idade
+        if sexo == 'M':
+            if n == 1:
+                maiorIdade = idade
+                nomeMaisvelho = nome
+            else:
+                if idade > maiorIdade:
+                    maiorIdade = idade
+                    nomeMaisvelho = nome      
+        if sexo == 'F':
+            if idade < 20:
+                contMaior20 += 1
+        
+    print(f'A média das idades é: {somaIdades/4:.2f} anos')
+    print(f'O homem mais velho é: {nomeMaisvelho} e tem {maiorIdade} anos')
+    print(f'Temos {contMaior20} mulheres com menos de 20 anos.')
+    
 if __name__ == '__main__':
     main()
